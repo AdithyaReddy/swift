8f3b7fc5a19b6f8b37e79faac523fbbcbb3bb908
David Farler <dfarler@apple.com>
2017-04-14 15:54:16 -0700

    [Syntax] Refactor AddSwiftSyntaxTableGen signatures
    Provide the syntax category, construct the filename, and return
    the output filename, for some definition of return in CMake.

--- cmake/modules/AddSwiftSyntaxTableGen.cmake
+++ cmake/modules/AddSwiftSyntaxTableGen.cmake
@@ -4,29 +4,35 @@ function(swift_add_syntax_generated_source category)
   cmake_parse_arguments(
     SYNTAX_GEN # prefix
     "" # options
-    "OUTPUT_DIR;ACTION;CATEGORY;TARGET_LANGUAGE" # single-value args
+    "OUTPUT_DIR;ACTION;TARGET_LANGUAGE" # single-value args
     "" # multi-value args
     ${ARGN})
+  precondition(category "Category is required")
   precondition(SYNTAX_GEN_OUTPUT_DIR "Output dir is required")
   precondition(SYNTAX_GEN_ACTION "Action is required")
-  precondition(SYNTAX_GEN_CATEGORY "Category is required")
   precondition(SYNTAX_GEN_TARGET_LANGUAGE "Target language is required")
+  set(SYNTAX_GEN_CATEGORY ${category})
 
-  set(filename ${category})
-  if (category IN_LIST SYNTAX_GEN_SYNTAX_CATEGORIES)
-    set(filename "${filename}Syntax")
+  set(out_filename ${SYNTAX_GEN_CATEGORY})
+  if (SYNTAX_GEN_CATEGORY IN_LIST SYNTAX_GEN_SYNTAX_CATEGORIES)
+    set(out_filename "${out_filename}Syntax")
   endif()
 
   if (${SYNTAX_GEN_ACTION} STREQUAL "interface")
-    set(filename "${filename}.h")
+    set(out_filename "${out_filename}.h")
   elseif (${SYNTAX_GEN_ACTION} STREQUAL "implementation")
-    set(filename "${filename}.cpp")
+    set(out_filename "${out_filename}.cpp")
   endif()
 
+  precondition(out_filename "Calculated output filename is empty?!")
+
   set(syntax_tblgen_tool "${CMAKE_BINARY_DIR}/${CMAKE_CFG_INTDIR}/bin/swift-syntax-tblgen")
-  set(syntax_gen_output_file "${SYNTAX_GEN_OUTPUT_DIR}/${filename}")
+  set(syntax_gen_output_file "${SYNTAX_GEN_OUTPUT_DIR}/${out_filename}")
   set(syntax_gen_td_file "${CMAKE_SOURCE_DIR}/include/swift/Syntax/Syntax.td")
   set(syntax_gen_include_dir "${CMAKE_SOURCE_DIR}/include/swift/Syntax")
+
+  # "return" this string to the caller. :roll-eyes:
+  set(out_filename ${syntax_gen_output_file} PARENT_SCOPE)
 
   add_custom_command_target(
     syntax_gen_output_file
